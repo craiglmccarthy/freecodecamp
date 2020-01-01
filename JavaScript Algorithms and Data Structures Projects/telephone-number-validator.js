cc3062f382704f69de8744303964320e6fb0619f
craiglmccarthy <47318414+craiglmccarthy@users.noreply.github.com>
2020-01-01 23:11:40 +0000

    Update telephone-number-validator.js

--- JavaScript Algorithms and Data Structures Projects/telephone-number-validator.js
+++ JavaScript Algorithms and Data Structures Projects/telephone-number-validator.js
@@ -2,4 +2,4 @@ function telephoneCheck(str) {
   let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
   return regex.test(str);
 }
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("555-555-5555"));
