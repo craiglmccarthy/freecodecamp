29bc4594d90bf81ed782a95b494d281989ebead2
craiglmccarthy <craiglmccarthy@gmail.com>
2020-01-06 08:39:37 +0000

    Amended comment

--- JavaScript Algorithms and Data Structures Projects/telephone-number-validator.js
+++ JavaScript Algorithms and Data Structures Projects/telephone-number-validator.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 function telephoneCheck(str) {
-  let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-  return regex.test(str);
-}
-telephoneCheck("555-555-5555");
-=======
-function telephoneCheck(str) {
-  # Regex found - https://www.regextester.com/98583
+  // Regex found - https://www.regextester.com/98583
   let regex = /^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/;
   return regex.test(str);
 }
 console.log(telephoneCheck("555-555-5555"));
->>>>>>> 5ef9eb8907258e01790fdfbdfe28d5272ea7bcfc
